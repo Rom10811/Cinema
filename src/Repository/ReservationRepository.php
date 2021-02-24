fd34c11c75e9d7e91d1f650d3312535a076230f5
Romain <romain.duciel08@gmail.com>
2021-02-24 11:23:21 +0100

    10 commit

--- src/Repository/ReservationRepository.php
+++ src/Repository/ReservationRepository.php
@@ -12,7 +12,6 @@ use http\QueryString;
 /**
  * @method Reservation|null find($id, $lockMode = null, $lockVersion = null)
  * @method Reservation|null findOneBy(array $criteria, array $orderBy = null)
- * @method Reservation[]    findAll()
  * @method Reservation[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ReservationRepository extends ServiceEntityRepository
@@ -20,6 +19,10 @@ class ReservationRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Reservation::class);
+    }
+
+    public function findAll(){
+        return $this->findBy(array(), array('id'=>'ASC'));
     }
 
     // /**
